f01aada750c9482a45999d8d401eca19c5778695
Brandon Weigand <brandonweigand@gmail.com>
2021-03-12 12:10:27 -0500

    test featurer build access files on github

---
+++ build.js
@@ -0,0 +1,59 @@
+var DATE = new Date;
+var build = {
+    "componets":[
+        "header",
+        "warrenty"
+    ],
+    "formats":{
+        "LICENCE":true,
+        "HTML":true,
+        "README":true
+    },
+    "vars":{
+        "ID":"406a923tr4",
+        "OWNER":"Brandon Weigand",
+        "LICENCE_URL":"https://github.com/BrandonWeigand/SAVCL/",
+        "MIN":0,
+        "MAX":9999,
+        "YEAR":DATE.getFullYear()
+    },
+    "files":[]
+}
+build.run=function(builddata={}){
+    builddata=Object.apply(build,builddata);
+}
+build.format={
+    LICENCE:function(builddata={}){
+
+    },
+    HTML:function(builddata={}){
+
+    },
+    README:function(builddata={}){
+
+    }
+};
+build.file={
+    get:function(file_url){
+        // get a file from the url
+        url="https://raw.githubusercontent.com/BrandonWeigand/web_metrics/master/debug.json?token=ADVK6DM3WZ7CXDIZ25XHCCDAJOQAS";
+
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => console.log(data));
+        
+        // parse the filedata
+
+        // store it in an array
+        
+        // return file id
+    },
+    sort:function(by='order',reverse=false){
+
+    },
+    merge:function(){// return an object of all the file data merged
+
+    }
+};
